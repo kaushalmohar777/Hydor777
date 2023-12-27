a5ebd5406b95d99fa8ef311a38b4a4559f1d67e3
DESKTOP-6AOUGLM\dell <kaushalmohar777@gmail.com>
2023-12-27 16:57:33 +0530

    getting product dynamic

--- app/components/SliderCard.tsx
+++ app/components/SliderCard.tsx
@@ -1,4 +1,5 @@
-
+// import { request, gql } from "graphql-request";
+// import React, { useEffect, useState } from 'react';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -6,6 +7,106 @@ import 'swiper/css/pagination';
 
 
 const SliderCard = () => {
+  
+
+
+
+
+  // const [productData, setProductData] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const graphqlEndpoint = 'https://0f3d49.myshopify.com/api/2023-12/graphql.json';
+  //     const accessToken = 'f06aedcf11ba46797bec07aa0cd9467b';
+
+  //     const graphqlQuery = `
+  //       {
+  //         product(id: "gid://shopify/Product/8893067493673") {
+  //           id
+  //           title
+  //           description
+  //           featuredImage {
+  //             id
+  //             url
+  //           }
+  //         }
+  //       }
+    //   `;
+
+    //   try {
+    //     const response = await fetch(graphqlEndpoint, {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         'X-Shopify-Storefront-Access-Token': accessToken,
+    //       },
+    //       body: JSON.stringify({ query: graphqlQuery }),
+    //     });
+
+    //     if (!response.ok) {
+    //       throw new Error(`GraphQL request failed with status ${response.status}`);
+    //     }
+
+    //     const data = await response.json();
+    //     console.log(data);
+    //     const fetchedProductData = data.data.product;
+    //     setProductData(fetchedProductData);
+    //   } catch (error) {
+    //     console.error('GraphQL request failed', error);
+    //   }
+    // };
+
+    // fetchData();
+  // }, []);
+  
+
+
+
+
+
+
+const graphqlEndpoint = 'https://0f3d49.myshopify.com/api/2023-12/graphql.json';
+const accessToken = 'f06aedcf11ba46797bec07aa0cd9467b';
+
+
+const graphqlQuery = `
+{
+  product(id: "gid://shopify/Product/8893067493673") {
+    id
+    title
+    description
+    featuredImage {
+      id
+      url
+    }
+  }
+}
+`;
+
+fetch(graphqlEndpoint, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Shopify-Storefront-Access-Token': accessToken,
+  },
+  body: JSON.stringify({ query: graphqlQuery }),
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`GraphQL request failed with status ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('GraphQL request failed', error);
+  });
+
+
+
+
   return (
     <div className='slider-component'>
         <h2>SliderCard</h2>
@@ -21,6 +122,23 @@ const SliderCard = () => {
       <SwiperSlide><img src="https://cdn.shopify.com/s/files/1/0844/9353/3481/files/card-img1.avif?v=1702562184" /></SwiperSlide>
       <SwiperSlide><img src="https://cdn.shopify.com/s/files/1/0844/9353/3481/files/card-img1.avif?v=1702562184" /></SwiperSlide>
     </Swiper>
+
+
+{/* <div>
+      {productData ? (
+        <div>
+          <h2>{productData.title}</h2>
+          <p>{productData.description}</p>
+          {productData.featuredImage && (
+            <img src={productData.featuredImage.url} alt={productData.title} />
+          )}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div> */}
+
+
     </div>
   )
 }
